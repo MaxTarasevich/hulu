9752216914e57a1964a36c2fe13a1a5603295652
Max <maxtarasevich@gmail.com>
2021-07-05 09:58:26 +0300

    change nav component margin

--- components/Nav.js
+++ components/Nav.js
@@ -4,7 +4,7 @@ import {useRouter} from "next/router"
 function Nav() {
     const router = useRouter();
     return (
-        <nav className="p-8 relative">
+        <nav className="mb-4 relative">
             <div className="flex  overflow-x-scroll scrollbar-hide px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 ">
                 {Object.entries(requests).map(([key, {title, url}]) => (
                     <h2 key={key} className="last:pr-24 cursor-pointer my-2 transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
@@ -16,7 +16,7 @@ function Nav() {
                 ))}
             </div>
             <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
-            <div className="absolute top-0 left-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
+            <div className="absolute top-0 left-0 bg-gradient-to-r from-[#06202A] h-10 w-1/12" />
         </nav>
     )
 }
